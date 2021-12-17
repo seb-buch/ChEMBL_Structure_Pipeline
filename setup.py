3d0664410fd0aa106de4981f02433a6b3dab6a32
Sébastien BUCHOUX <Sebastien.Buchoux@evotec.com>
2021-12-17 16:18:00 +0100

    Updated setup.py to get the version number without import. Version number bumped to 1.0.1
    Prior to this commit the setup.py script imported the package to get its version.
    This import triggered some code execution that would ultimately failed if rdkit was not installed.

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup
-import chembl_structure_pipeline
+from pathlib import Path
+
+def get_version_from_init(folder: Path=None) -> str:
+      if folder is None:
+            folder = Path(__file__).parent
+      
+      init_file = folder / 'chembl_structure_pipeline' / '__init__.py'
+
+      with open(init_file) as fp:
+            for line in fp:
+                  if line.startswith('__version__'):
+                        return line.split('=')[1].strip(' "\n')
+      
+      raise RuntimeError(f"Could not find version string in {init_file}")
 
 setup(name='chembl_structure_pipeline',
-      version=chembl_structure_pipeline.__version__,
+      version=get_version_from_init(),
       description='ChEMBL Structure Pipeline',
       url='https://www.ebi.ac.uk/chembl/',
       author='Greg Landrum',
